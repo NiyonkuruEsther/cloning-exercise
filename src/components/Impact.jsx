a9de805f7acc96e7183054a54c9fbbb7737978c1
estherniyo <estherniyonkuru123@gmail.com>
2023-02-20 21:36:25 +0200

    progressing

--- src/components/Impact.jsx
+++ src/components/Impact.jsx
@@ -9,10 +9,11 @@ const Impact = () => {
         Impact
       </p>
       <div className="max-h-[700px] max-w-[1600px] bg-center h-[700px] flex flex-col w-[1600px] bg-cover justify-end bg-[url('https://assets.vu.nl/d8b6f1f5-816c-005b-1dc1-e363dd7ce9a5/883cc0a7-cdf9-41aa-b410-2bce37f7a554/001_VU_com_Davide-Ianuzzi_PeGe_2022_PG006556_lowres%5B28%5D.jpg?w=1250&h=600&fit=clip&rect=336%2C153%2C1664%2C799&fm=jpg&auto=format&q=40&dpr=2')]">
-        <div className="bg-white w-fit curve flex flex-col gap-4 py-4 px-6 self-center -mb-24">
-          <h1 className="text-red text-3xl ">Enterprenuership on VU</h1>
-          <p className="text">
-            Doing business as a student? View the options here.
+        <div className="bg-white w-fit curve max-w-3xl mx-auto flex flex-col gap-4 px-16 self-center -mb-24 py-12">
+          <h1 className="text-red text-3xl mt-12">Enterprenuership on VU</h1>
+          <p className="text-xl">
+            An interview with entrepreneurial scientist Davide Iannuzzi, Chief
+            Impact Officer of Vrije Universiteit Amsterdam{" "}
           </p>
           <div className="flex gap-4 items-center">
             <p className="text-xl font-semibold">Read more</p>
